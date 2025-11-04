847d3f14f18b30e36b26091a48d1e6dc1dbe7302
Ayşe Bingöl <aysebingol@itu.edu.tr>
2025-11-04 11:40:35 +0300

    goruntu ekranda gosterme

--- 12.py
+++ 12.py
@@ -1,9 +1,23 @@
 import cv2
 
-for i in range(5):  
-    cap = cv2.VideoCapture(i)
-    if cap.isOpened():
-        print(f"Kamera {i} açıldı!")
-        cap.release()
-    else:
-        print(f"Kamera {i} açılamadı.")
+video_path = "test.mp4"  
+cap = cv2.VideoCapture(video_path)
+
+if not cap.isOpened():
+    print("Video açılamadı.")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Video bitti veya okunamadı.")
+        break
+
+    cv2.imshow("Kamera Simülasyonu", frame)
+
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
